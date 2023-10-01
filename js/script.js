08fcb258907b0cecad92d8d8f0277a7c7ea3c91b
Vinicius Parabotchey <viniciusparabotchey@gmail.com>
2023-10-01 16:05:59 -0300

    ajustes nos pontos

--- js/script.js
+++ js/script.js
@@ -1,29 +1,49 @@
-const canvas = document.querySelector("canvas")
+const canvas = document.getElementById("myCanvas")
 const ctx = canvas.getContext("2d")
+var h1 = document.querySelector("h1")
 
 const size = 30
 
 const snake = [{x: 270, y: 240}]
 
+function randomNumber(min, max) {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+function randomPosition() {
+    const number = randomNumber(0, canvas.width - size)
+    return Math.round(number / 30) * 30
+}
+
+function randomColor() {
+    const red = randomNumber(0, 255)
+    const green = randomNumber(0, 255)
+    const blue = randomNumber(0, 255)
+
+    return  `rgb(${red}, ${green}, ${blue})`
+}
+
+h1.innerText = randomColor()
+
 const food = {
-    x: 90,
-    y: 90,
-    color: "yellow"
+    x: randomPosition(),
+    y: randomPosition(),
+    color: randomColor()
 }
 
 let direction, loopId
 
-const drawFood = () => {
+function drawFood() {
     const {x, y, color} = food
 
     ctx.shadowColor = color
     ctx.shadowBlur = 6
     ctx.fillStyle = color
-    ctx.fillRect = (x, y, size, size)
+    ctx.fillRect(x, y, size, size)
     ctx.shadowBlur = 0
 }
 
-const drawSnake = () => {
+function drawSnake() {
     ctx.fillStyle = "#ddd"
 
     snake.forEach((position, index) => {
@@ -32,11 +52,11 @@ const drawSnake = () => {
             ctx.fillStyle = "white"
         }
         
-        ctx.fillRect()
+        ctx.fillRect(position.x, position.y, size, size)
     })
 }
 
-const moveSnake = () => {
+function moveSnake() {
     if (!direction) return
 
     const head = snake[snake.length -1]
@@ -60,7 +80,7 @@ const moveSnake = () => {
     snake.shift()
 }
 
-const drawGrid = () => {
+function drawGrid() {
     ctx.lineWidth = 1
     ctx.strokeStyle = "#191919"
     
@@ -79,7 +99,14 @@ const drawGrid = () => {
     
 }
 
-const gameLoop =() => {
+function checkEat() {
+    const head = snake[snake.length - 1]
+    if (head.x == food.x && head.y == food.y) {
+        snake.push
+    }
+}
+
+function gameLoop() {
     clearInterval(loopId)
 
     ctx.clearRect(0, 0, 600, 600)
